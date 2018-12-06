b5285e7647339a51b19da2acdb815e1a0f9e25b7
Bắc Lục <45648505+NosuGurin@users.noreply.github.com>
2018-12-06 17:04:07 +0700

    Commit 1

---
+++ BarcodeDemo/BarcodeDemo/BarcodeDemo/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace BarcodeDemo
+{
+	public partial class MainPage : ContentPage
+	{
+	    ZXingScannerPage scanPage;
+        public MainPage()
+		{
+			InitializeComponent();
+		}
+
+	    private async void Button_OnClicked(object sender, EventArgs e)
+	    {
+	        
+	            scanPage = new ZXingScannerPage();
+	            scanPage.OnScanResult += (result) => {
+	                scanPage.IsScanning = false;
+
+	                Device.BeginInvokeOnMainThread(() => {
+	                    Navigation.PopModalAsync();
+	                    DisplayAlert("Scanned Barcode", result.Text, "OK");
+	                });
+	            };
+
+	            await Navigation.PushModalAsync(scanPage);
+	        
+        }
+	}
+}
